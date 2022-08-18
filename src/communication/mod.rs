a7f22444e29712ff64057a320caf6425aeeaea70
tehforsch <mail@tonipeter.de>
2022-08-18 11:11:17 +0200

    refactor communication/mod.rs

--- src/communication/mod.rs
+++ src/communication/mod.rs
@@ -5,37 +5,49 @@ mod sized_communicator;
 mod sync_communicator;
 mod world_communicator;
 
-#[cfg(not(feature = "local"))]
-mod mpi_world;
-
-#[cfg(feature = "local")]
-mod local;
-
 pub use data_by_rank::DataByRank;
 pub use identified::Identified;
 pub use sized_communicator::SizedCommunicator;
 pub use world_communicator::WorldCommunicator;
 
-#[cfg(feature = "local")]
-pub use self::local::get_local_communicators;
-
 pub type Rank = mpi::Rank;
 
 #[cfg(feature = "local")]
-pub type ExchangeCommunicator<T> =
-    exchange_communicator::ExchangeCommunicator<self::local::LocalCommunicator<T>, T>;
-#[cfg(feature = "local")]
-pub type SyncCommunicator<T> =
-    sync_communicator::SyncCommunicator<self::local::LocalCommunicator<Identified<T>>, T>;
-
-#[cfg(not(feature = "local"))]
-pub type ExchangeCommunicator<T> =
-    exchange_communicator::ExchangeCommunicator<self::mpi_world::MpiWorld<T>, T>;
-#[cfg(not(feature = "local"))]
-pub type SyncCommunicator<T> =
-    sync_communicator::SyncCommunicator<self::mpi_world::MpiWorld<Identified<T>>, T>;
+mod local;
 
 #[cfg(feature = "local")]
-pub type Communicator<T> = self::local::LocalCommunicator<T>;
+pub use local_reexport::*;
+
+#[cfg(feature = "local")]
+#[path = ""]
+mod local_reexport {
+    use super::identified::Identified;
+
+    pub type ExchangeCommunicator<T> =
+        super::exchange_communicator::ExchangeCommunicator<super::local::LocalCommunicator<T>, T>;
+    pub type SyncCommunicator<T> = super::sync_communicator::SyncCommunicator<
+        super::local::LocalCommunicator<Identified<T>>,
+        T,
+    >;
+    pub type Communicator<T> = super::local::LocalCommunicator<T>;
+
+    pub use super::local::get_local_communicators;
+}
+
 #[cfg(not(feature = "local"))]
-pub type Communicator<T> = self::mpi_world::MpiWorld<T>;
+mod mpi_world;
+
+#[cfg(not(feature = "local"))]
+pub use mpi_reexport::*;
+
+#[cfg(not(feature = "local"))]
+#[path = ""]
+mod mpi_reexport {
+    use super::identified::Identified;
+    pub type ExchangeCommunicator<T> =
+        super::exchange_communicator::ExchangeCommunicator<super::mpi_world::MpiWorld<T>, T>;
+    pub type SyncCommunicator<T> =
+        super::sync_communicator::SyncCommunicator<super::mpi_world::MpiWorld<Identified<T>>, T>;
+
+    pub type Communicator<T> = super::mpi_world::MpiWorld<T>;
+}
