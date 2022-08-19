8ebda42fe4df77f1c5f9a70056c1c304b29b889a
tehforsch <mail@tonipeter.de>
2022-08-19 21:05:44 +0200

    allow deserializing units in parameter files

--- src/units/quantities_and_units.rs
+++ src/units/quantities_and_units.rs
@@ -83,12 +83,20 @@ impl<const D: Dimension> Sum for Quantity<glam::Vec2, D> {
 }
 
 macro_rules! unit_functions {
-    ($storage_type:ty, $($const: ident, $quantity:ident, $($dimension_name: ident: $dimension: literal),*, {$($unit:ident, $factor:literal),+}),+) => {
+    ($storage_type:ty, $($const: ident, $quantity:ident, $($dimension_name: ident: $dimension: literal),*, {$($unit:ident, $factor:literal, $($unit_symbol:literal)?),+}),+) => {
         use super::Dimension;
         use super::Quantity;
         use super::NONE;
+        pub const UNIT_NAMES: &[(Dimension, &str, f32)] = &[
         $(
-
+            $(
+                $(
+                    ($const, $unit_symbol, $factor),
+                )*
+            )*
+        )*
+        ];
+        $(
             const $const: Dimension = Dimension {
                 $(
                     $dimension_name: $dimension,
@@ -110,32 +118,32 @@ macro_rules! implement_storage_type {
         unit_functions!($type,
                     DIMENSIONLESS, Dimensionless, length: 0,
                     {
-                        dimensionless, 1.0
+                        dimensionless, 1.0, ""
                     },
                     LENGTH, Length, length: 1,
                     {
-                        meter, 1.0,
-                        kilometer, 1000.0
+                        meter, 1.0, "m",
+                        kilometer, 1000.0, "km"
                     },
                     TIME, Time, time: 1,
                     {
-                        second, 1.0
+                        second, 1.0, "s"
                     },
                     VELOCITY, Velocity, length: 1, time: -1,
                     {
-                        meters_per_second, 1.0
+                        meters_per_second, 1.0,
                     },
                     MASS, Mass, mass: 1,
                     {
-                        kilogram, 1.0
+                        kilogram, 1.0, "kg"
                     },
                     ACCELERATION, Acceleration, length: 1, time: -2,
                     {
-                        meters_per_second_squared, 1.0
+                        meters_per_second_squared, 1.0,
                     },
                     FORCE, Force, mass: 1, length: 1, time: -2,
                     {
-                        newton, 1.0
+                        newton, 1.0, "N"
                     }
                     );
     }
