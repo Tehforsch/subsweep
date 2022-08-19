882ca18b654319f243f881eabdb594ec3f4c090e
tehforsch <mail@tonipeter.de>
2022-08-19 21:52:00 +0200

    play around with initial conditions

--- src/initial_conditions.rs
+++ src/initial_conditions.rs
@@ -24,18 +24,32 @@ fn spawn_particles_system(mut commands: Commands, rank: Res<Rank>) {
     if *rank != 0 {
         return;
     }
-    let n_particles = 10;
+    let n_particles = 150;
     for _ in 0..n_particles {
-        let x = rand::thread_rng().gen_range(-3.0..3.0);
-        let y = rand::thread_rng().gen_range(-3.0..3.0);
+        let x = rand::thread_rng().gen_range(-5.0..-4.0);
+        let y = rand::thread_rng().gen_range(-1.0..1.0);
         let pos = meter(Vec2::new(x, y));
-        let x = rand::thread_rng().gen_range(-3.0..3.0);
-        let y = rand::thread_rng().gen_range(-3.0..3.0);
-        let vel = meters_per_second(Vec2::new(x, y)) * 0.0;
+        let x = 0.0;
+        let y = 0.1;
+        let vel = meters_per_second(Vec2::new(x, y)) * 1.0;
         commands.spawn().insert_bundle(LocalParticleBundle::new(
             Position(pos),
             Velocity(vel),
-            Mass(kilogram(1000000.0)),
+            Mass(kilogram(10000000.0)),
+        ));
+    }
+
+    for _ in 0..n_particles {
+        let x = rand::thread_rng().gen_range(4.0..5.0);
+        let y = rand::thread_rng().gen_range(-1.0..1.0);
+        let pos = meter(Vec2::new(x, y));
+        let x = 0.0;
+        let y = -0.1;
+        let vel = meters_per_second(Vec2::new(x, y)) * 1.0;
+        commands.spawn().insert_bundle(LocalParticleBundle::new(
+            Position(pos),
+            Velocity(vel),
+            Mass(kilogram(10000000.0)),
         ));
     }
 }
