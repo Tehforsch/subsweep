f9dea26060eb06e04b4bd3a3816e7b981365a84b
tehforsch <mail@tonipeter.de>
2022-08-22 21:47:01 +0200

    move to_integer_pos function into test module

--- src/domain/peano_hilbert.rs
+++ src/domain/peano_hilbert.rs
@@ -3,19 +3,19 @@ use crate::units::VecLength;
 
 // These values are for 3d, but I'll use them for 2D as well, since it
 // doesn't really matter there anyways
-const NUM_BITS_N_bitsPER_DIMENSION: i64 = 21;
-const NUM_SUBDIVISIONS: i64 = 2i64.pow(21);
+const NUM_BITS_PER_DIMENSION: u32 = 21;
+const NUM_SUBDIVISIONS: u64 = 2u64.pow(NUM_BITS_PER_DIMENSION);
 
-#[derive(Clone, Debug)]
-pub struct PeanoHilbertKey(i64);
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct PeanoHilbertKey(u64);
 
-pub fn get_integer_position(extents: &Extent, pos: &VecLength) -> (i64, i64) {
+pub fn get_integer_position(extents: &Extent, pos: &VecLength) -> (u64, u64) {
     let shifted = *pos - extents.lower_left();
     let fraction = shifted / extents.side_lengths();
     let fraction = fraction.value();
     (
-        (fraction.x * NUM_SUBDIVISIONS as f32) as i64,
-        (fraction.y * NUM_SUBDIVISIONS as f32) as i64,
+        (fraction.x * NUM_SUBDIVISIONS as f32) as u64,
+        (fraction.y * NUM_SUBDIVISIONS as f32) as u64,
     )
 }
 
@@ -26,12 +26,12 @@ impl PeanoHilbertKey {
     }
 
     // Source: https://en.wikipedia.org/wiki/Hilbert_curve
-    fn from_integer_pos((mut x, mut y): (i64, i64)) -> Self {
+    fn from_integer_pos((mut x, mut y): (u64, u64)) -> Self {
         let mut s = NUM_SUBDIVISIONS / 2;
         let mut d = 0;
         while s > 0 {
-            let rx = ((x & s) > 0) as i64;
-            let ry = ((y & s) > 0) as i64;
+            let rx = ((x & s) > 0) as u64;
+            let ry = ((y & s) > 0) as u64;
             d += s * s * ((3 * rx) ^ ry);
             Self::rot(NUM_SUBDIVISIONS, &mut x, &mut y, rx, ry);
             s /= 2;
@@ -39,24 +39,7 @@ impl PeanoHilbertKey {
         Self(d)
     }
 
-    fn to_integer_pos(&self) -> (i64, i64) {
-        let mut t = self.0;
-        let mut x = 0;
-        let mut y = 0;
-        let mut s = 1;
-        while s < NUM_SUBDIVISIONS {
-            let rx = 1 & (t / 2);
-            let ry = 1 & (t ^ rx);
-            Self::rot(s, &mut x, &mut y, rx, ry);
-            x += s * rx;
-            y += s * ry;
-            t /= 4;
-            s *= 2;
-        }
-        (x, y)
-    }
-
-    fn rot(n: i64, x: &mut i64, y: &mut i64, rx: i64, ry: i64) {
+    fn rot(n: u64, x: &mut u64, y: &mut u64, rx: u64, ry: u64) {
         if ry == 0 {
             if rx == 1 {
                 *x = (n - 1) - *x;
@@ -69,8 +52,27 @@ impl PeanoHilbertKey {
 
 #[cfg(test)]
 mod tests {
+    use super::NUM_SUBDIVISIONS;
     use crate::domain::peano_hilbert::PeanoHilbertKey;
 
+    impl PeanoHilbertKey {
+        fn to_integer_pos(&self) -> (u64, u64) {
+            let mut t = self.0;
+            let mut x = 0;
+            let mut y = 0;
+            let mut s = 1;
+            while s < NUM_SUBDIVISIONS {
+                let rx = 1 & (t / 2);
+                let ry = 1 & (t ^ rx);
+                Self::rot(s, &mut x, &mut y, rx, ry);
+                x += s * rx;
+                y += s * ry;
+                t /= 4;
+                s *= 2;
+            }
+            (x, y)
+        }
+    }
     #[test]
     fn peano_hilbert_map_is_isomorphic() {
         for x in 0..30 {
