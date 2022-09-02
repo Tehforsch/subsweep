4e13980c829732c15673f36cd37a458bc14326ed
tehforsch <mail@tonipeter.de>
2022-09-02 22:47:13 +0200

    disable cuda support to supress warning message

--- runMpi.sh
+++ runMpi.sh
@@ -1,3 +1,3 @@
 killall tenet
-cargo build && mpirun -n 4 ~/.cargo-target/debug/tenet $@
+cargo build && mpirun --mca opal_warn_on_missing_libcuda 0 -n 4 ~/.cargo-target/debug/tenet $@
 killall tenet
