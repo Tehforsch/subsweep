48ce9c95cdf746e946fbbfa9b9a36eddbe1e7961
tehforsch <mail@tonipeter.de>
2022-09-06 18:49:23 +0200

    initial commit for output plugin

---
+++ src/output/mod.rs
@@ -0,0 +1,34 @@
+use std::marker::PhantomData;
+
+use bevy::prelude::Component;
+use bevy::prelude::Plugin;
+use bevy::prelude::Query;
+use bevy::prelude::StageLabel;
+use bevy::prelude::SystemStage;
+
+use crate::physics::PhysicsStages;
+
+#[derive(StageLabel)]
+enum OutputStages {
+    Output,
+}
+
+#[derive(Default)]
+struct OutputPlugin<T> {
+    _marker: PhantomData<T>,
+}
+
+impl<T: Component + Sync + Send + 'static> Plugin for OutputPlugin<T> {
+    fn build(&self, app: &mut bevy::prelude::App) {
+        app.add_stage_after(
+            PhysicsStages::Gravity,
+            OutputStages::Output,
+            SystemStage::parallel(),
+        );
+        app.add_system_to_stage(OutputStages::Output, Self::output_system);
+    }
+}
+
+impl<T: Component> OutputPlugin<T> {
+    fn output_system(query: Query<&T>) {}
+}
