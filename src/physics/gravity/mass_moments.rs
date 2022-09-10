2048dcecf593f8cb3ca226e59a974e6e2a3936de
tehforsch <mail@tonipeter.de>
2022-09-10 14:04:49 +0200

    add debug print for mass moments

--- src/physics/gravity/mass_moments.rs
+++ src/physics/gravity/mass_moments.rs
@@ -1,10 +1,13 @@
+use std::iter::Sum;
+
 use super::ParticleData;
 use crate::domain::quadtree::NodeDataType;
 use crate::units::Mass;
+use crate::units::Vec2Length;
 use crate::units::VecLength;
 use crate::units::VecLengthMass;
 
-#[derive(Default, Debug)]
+#[derive(Default)]
 pub struct MassMoments {
     total: Mass,
     weighted_position_sum: VecLengthMass,
@@ -22,6 +25,12 @@ impl MassMoments {
         }
         self.weighted_position_sum / self.total
     }
+
+    pub fn add_mass_at(&mut self, pos: &VecLength, mass: &Mass) {
+        self.count += 1;
+        self.total += *mass;
+        self.weighted_position_sum += *pos * *mass;
+    }
 }
 
 impl NodeDataType<ParticleData> for MassMoments {
@@ -29,5 +38,26 @@ impl NodeDataType<ParticleData> for MassMoments {
         self.count += 1;
         self.total += data.mass;
         self.weighted_position_sum += *pos * data.mass;
+    }
+}
+
+impl Sum<(Mass, Vec2Length)> for MassMoments {
+    fn sum<I: Iterator<Item = (Mass, Vec2Length)>>(iter: I) -> Self {
+        let mut s = Self::default();
+        for (mass, pos) in iter {
+            s.add_mass_at(&pos, &mass);
+        }
+        s
+    }
+}
+
+impl std::fmt::Debug for MassMoments {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Moments({:.3?} @ {:.3?})",
+            self.total(),
+            self.center_of_mass()
+        )
     }
 }
