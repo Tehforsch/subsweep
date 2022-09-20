ea35bd6ccba902e154b3a2c0bfcc974f1193dd9a
tehforsch <mail@tonipeter.de>
2022-09-20 17:06:51 +0200

    move simulation to plugin_utils

--- src/plugin_utils/mod.rs
+++ src/plugin_utils/mod.rs
@@ -1,13 +1,208 @@
-mod simulation;
 mod tenet_plugin;
 
 use std::collections::HashSet;
 
-pub use simulation::Simulation;
+use bevy::ecs::event::Event;
+use bevy::ecs::schedule::IntoSystemDescriptor;
+use bevy::ecs::system::Resource;
+use bevy::prelude::warn;
+use bevy::prelude::App;
+use bevy::prelude::Mut;
+use bevy::prelude::Plugin;
+use bevy::prelude::PluginGroup;
+use bevy::prelude::Stage;
+use bevy::prelude::StageLabel;
+use bevy::prelude::World;
+use serde::Deserialize;
 pub use tenet_plugin::TenetPlugin;
+
+use crate::communication::WorldRank;
+use crate::named::Named;
+use crate::parameters::ParameterPlugin;
+use crate::parameters::Parameters;
 
 #[derive(Default)]
 struct RunOnceLabels(HashSet<&'static str>);
 
 #[derive(Default)]
 struct AlreadyAddedLabels(HashSet<&'static str>);
+#[derive(Default)]
+
+pub struct Simulation(pub App);
+
+impl Simulation {
+    pub fn new() -> Self {
+        Self(App::new())
+    }
+
+    pub fn add_plugin<T: Sync + Send + 'static + TenetPlugin>(&mut self, plugin: T) -> &mut Self {
+        if !plugin.should_build(self) {
+            return self;
+        }
+        self.run_once::<T>(|sim| {
+            plugin.build_once_everywhere(sim);
+            if !sim.has_world_rank() {
+            } else if sim.on_main_rank() {
+                plugin.build_once_on_main_rank(sim);
+            } else {
+                plugin.build_once_on_other_ranks(sim);
+            }
+        });
+        plugin.build_everywhere(self);
+        if !self.has_world_rank() {
+        } else if self.on_main_rank() {
+            plugin.build_on_main_rank(self);
+        } else {
+            plugin.build_on_other_ranks(self);
+        }
+        self
+    }
+
+    pub fn add_stage_after<S: Stage>(
+        &mut self,
+        target: impl StageLabel,
+        label: impl StageLabel,
+        stage: S,
+    ) -> &mut Self {
+        self.0.add_stage_after(target, label, stage);
+        self
+    }
+
+    pub fn add_system<Params>(&mut self, system: impl IntoSystemDescriptor<Params>) -> &mut Self {
+        self.0.add_system(system);
+        self
+    }
+
+    pub fn insert_resource<R: Resource>(&mut self, resource: R) -> &mut Self {
+        self.0.insert_resource(resource);
+        self
+    }
+
+    pub fn insert_non_send_resource<R: 'static>(&mut self, resource: R) -> &mut Self {
+        self.0.insert_non_send_resource(resource);
+        self
+    }
+
+    pub fn add_bevy_plugin<T: Plugin>(&mut self, plugin: T) -> &mut Self {
+        self.0.add_plugin(plugin);
+        self
+    }
+
+    pub fn add_bevy_plugins<T: PluginGroup>(&mut self, group: T) -> &mut Self {
+        self.0.add_plugins(group);
+        self
+    }
+
+    pub fn add_system_to_stage<Params>(
+        &mut self,
+        stage_label: impl StageLabel,
+        system: impl IntoSystemDescriptor<Params>,
+    ) -> &mut Self {
+        self.0.add_system_to_stage(stage_label, system);
+        self
+    }
+
+    pub fn add_startup_system_to_stage<Params>(
+        &mut self,
+        stage_label: impl StageLabel,
+        system: impl IntoSystemDescriptor<Params>,
+    ) -> &mut Self {
+        self.0.add_startup_system_to_stage(stage_label, system);
+        self
+    }
+
+    pub fn add_startup_system<Params>(
+        &mut self,
+        system: impl IntoSystemDescriptor<Params>,
+    ) -> &mut Self {
+        self.0.add_startup_system(system);
+        self
+    }
+
+    pub fn add_event<T>(&mut self) -> &mut Self
+    where
+        T: Event,
+    {
+        self.0.add_event::<T>();
+        self
+    }
+
+    pub fn run(&mut self) {
+        self.0.run()
+    }
+
+    pub fn update(&mut self) {
+        self.0.update()
+    }
+
+    pub fn get_resource<T: Sync + Send + 'static>(&self) -> Option<&T> {
+        self.0.world.get_resource::<T>()
+    }
+
+    pub fn get_resource_mut<T: Sync + Send + 'static>(&mut self) -> Option<Mut<T>> {
+        self.0.world.get_resource_mut::<T>()
+    }
+
+    pub fn unwrap_resource<T: Sync + Send + 'static>(&self) -> &T {
+        self.0.world.get_resource::<T>().unwrap()
+    }
+
+    pub fn unwrap_resource_mut<T: Sync + Send + 'static>(&mut self) -> Mut<T> {
+        self.0.world.get_resource_mut::<T>().unwrap()
+    }
+
+    pub fn unwrap_non_send_resource_mut<R: 'static>(&mut self) -> Mut<'_, R> {
+        self.0.world.non_send_resource_mut::<R>()
+    }
+
+    pub fn get_resource_or_insert_with<R: Resource>(
+        &mut self,
+        func: impl FnOnce() -> R,
+    ) -> Mut<'_, R> {
+        self.0.world.get_resource_or_insert_with(func)
+    }
+
+    pub fn contains_resource<T: Sync + Send + 'static>(&self) -> bool {
+        self.get_resource::<T>().is_some()
+    }
+
+    pub fn world(&mut self) -> &mut World {
+        &mut self.0.world
+    }
+
+    /// Panics if a named item was (accidentally) added twice
+    pub fn panic_if_already_added<P: Named>(&mut self) {
+        let mut labels = self.get_resource_or_insert_with(AlreadyAddedLabels::default);
+        if !labels.0.insert(P::name()) {
+            panic!("Added twice: {}", P::name())
+        }
+    }
+
+    fn has_world_rank(&self) -> bool {
+        if !self.contains_resource::<WorldRank>() {
+            warn!("World rank not present during plugin initialization, this should only happen in tests");
+            false
+        } else {
+            true
+        }
+    }
+
+    pub fn on_main_rank(&self) -> bool {
+        self.unwrap_resource::<WorldRank>().is_main()
+    }
+
+    pub fn run_once<P: Named>(&mut self, f: impl Fn(&mut Simulation)) {
+        let mut labels = self.get_resource_or_insert_with(RunOnceLabels::default);
+        if labels.0.insert(P::name()) {
+            f(self);
+        }
+    }
+
+    pub fn add_parameters<T>(&mut self, name: &str) -> T
+    where
+        T: Sync + Send + 'static + Clone + Parameters + for<'de> Deserialize<'de>,
+    {
+        self.add_plugin(ParameterPlugin::<T>::new(name));
+        self.unwrap_resource::<T>().clone()
+    }
+}
