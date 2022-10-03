2a8115d8ece6701e290dba54f916695ea96fe491
tehforsch <mail@tonipeter.de>
2022-10-03 13:05:35 +0200

    remove num_threads from command line options

--- src/command_line_options.rs
+++ src/command_line_options.rs
@@ -5,8 +5,6 @@ use clap::Parser;
 #[derive(Parser, Debug, Clone)]
 #[clap(author, version, about, long_about = None)]
 pub struct CommandLineOptions {
-    #[cfg(not(feature = "mpi"))]
-    pub num_threads: usize,
     pub parameter_file_path: Option<PathBuf>,
     #[clap(long)]
     pub headless: Option<bool>,
