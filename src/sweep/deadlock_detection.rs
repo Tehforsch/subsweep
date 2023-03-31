51bb31fa1c8e9e5ea55bc320588067f36fc5c5b9
tehforsch <mail@tonipeter.de>
2023-03-31 12:10:10 +0200

    improve deadlock detection print

--- src/sweep/deadlock_detection.rs
+++ src/sweep/deadlock_detection.rs
@@ -2,24 +2,64 @@ use bevy::prelude::warn;
 use bevy::utils::StableHashSet;
 use mpi::traits::Equivalence;
 
+use super::timestep_level::TimestepLevel;
 use super::Sweep;
 use crate::communication::exchange_communicator::ExchangeCommunicator;
 use crate::communication::DataByRank;
 use crate::communication::MpiWorld;
+use crate::communication::Rank;
 use crate::communication::SizedCommunicator;
 use crate::grid::ParticleType;
 use crate::prelude::ParticleId;
 
 const DEADLOCK_DETECTION_TAG: i32 = 99123151;
 
-#[derive(Clone, Equivalence, PartialEq, Eq, PartialOrd, Ord, Debug, Hash)]
+#[derive(Clone, Equivalence, PartialOrd, Ord, Debug, PartialEq, Eq, Hash)]
 struct Dependency {
-    p1: ParticleId,
-    p2: ParticleId,
+    p1: ParticleInfo,
+    p2: ParticleInfo,
+}
+
+#[derive(Clone, Equivalence, PartialEq, Eq, PartialOrd, Ord, Debug, Hash)]
+struct ParticleInfo {
+    rank: Rank,
+    id: ParticleId,
+    level: TimestepLevel,
+}
+
+impl std::fmt::Display for ParticleInfo {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "(rank={:>3} id={:>6} level={:>2})",
+            self.rank, self.id, self.level.0
+        )
+    }
 }
 
 impl<'a> Sweep<'a> {
-    pub fn check_deadlock(&mut self) {
+    fn get_dependency(
+        &self,
+        p1: ParticleId,
+        rank1: Rank,
+        p2: ParticleId,
+        rank2: Rank,
+    ) -> Dependency {
+        Dependency {
+            p1: ParticleInfo {
+                id: p1,
+                level: self.levels[&p1],
+                rank: rank1,
+            },
+            p2: ParticleInfo {
+                id: p2,
+                level: self.levels[&p2],
+                rank: rank2,
+            },
+        }
+    }
+
+    fn get_dependencies(&self) -> DataByRank<Vec<Dependency>> {
         let mut dependencies: DataByRank<Vec<_>> =
             DataByRank::from_communicator(&self.communicator);
 
@@ -27,24 +67,22 @@ impl<'a> Sweep<'a> {
             for (_, neighbour) in cell.neighbours.iter() {
                 match neighbour {
                     ParticleType::Remote(neigh) => {
-                        let rank = neigh.rank;
-                        let dep = if *id < neigh.id {
-                            Dependency {
-                                p1: *id,
-                                p2: neigh.id,
-                            }
+                        let dep = if neigh.rank > self.communicator.rank() {
+                            self.get_dependency(*id, self.communicator.rank(), neigh.id, neigh.rank)
                         } else {
-                            Dependency {
-                                p2: *id,
-                                p1: neigh.id,
-                            }
+                            self.get_dependency(neigh.id, neigh.rank, *id, self.communicator.rank())
                         };
-                        dependencies[rank].push(dep);
+                        dependencies[neigh.rank].push(dep);
                     }
                     _ => {}
                 }
             }
         }
+        dependencies
+    }
+
+    pub fn check_deadlock(&mut self) {
+        let dependencies = self.get_dependencies();
         let w = MpiWorld::new(DEADLOCK_DETECTION_TAG);
         let mut ex: ExchangeCommunicator<Dependency> = ExchangeCommunicator::from(w);
         let received = ex.exchange_all(dependencies.clone());
@@ -55,13 +93,9 @@ impl<'a> Sweep<'a> {
             if d1 != d2 {
                 if self.communicator.rank() < *rank {
                     println!("On rank {}:", self.communicator.rank());
-                    for dep in d1.difference(&d2) {
-                        println!("{:<6} <-> {:<6}", dep.p1, dep.p2);
-                    }
+                    print_diff(&d1, &d2);
                     println!("On rank {}:", rank);
-                    for dep in d2.difference(&d1) {
-                        println!("{:<6} <-> {:<6}", dep.p1, dep.p2);
-                    }
+                    print_diff(&d2, &d1);
                 }
                 panic!(
                     "Found {} different dependencies",
@@ -69,5 +103,13 @@ impl<'a> Sweep<'a> {
                 );
             }
         }
+    }
+}
+
+fn print_diff(set1: &StableHashSet<Dependency>, set2: &StableHashSet<Dependency>) {
+    let mut diff: Vec<_> = set1.difference(set2).cloned().collect();
+    diff.sort();
+    for dep in diff.into_iter() {
+        println!("{:<6} <-> {:<6}", dep.p1, dep.p2);
     }
 }
