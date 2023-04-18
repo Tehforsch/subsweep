c72b2a7f666ef5f4b3c2f2760de88516dba71542
tehforsch <mail@tonipeter.de>
2023-04-18 12:16:27 +0200

    read in cosmological units in arepo postprocess

--- examples/arepo_postprocess/unit_reader.rs
+++ examples/arepo_postprocess/unit_reader.rs
@@ -20,21 +20,29 @@ impl ArepoUnitReader {
     pub fn new(cosmology: Cosmology) -> Self {
         Self { cosmology }
     }
-}
 
-impl UnitReader for ArepoUnitReader {
-    fn read_scale_factor(&self, set: &Dataset) -> f64 {
-        let dimension = self.read_dimension(set);
-        let cgs_to_si = 0.01f64.powi(dimension.length) * 0.001f64.powi(dimension.mass);
-        let cgs: f64 = set
-            .attr(SCALE_FACTOR_IDENTIFIER)
-            .expect("No scale factor in dataset")
-            .read_scalar()
-            .unwrap();
-        cgs_to_si * cgs
+    fn get_cosmological_scale_factor(&self, dimension: &Dimension) -> f64 {
+        self.check_cosmology_available();
+        match self.cosmology {
+            Cosmology::Cosmological { a, h } => a.powi(dimension.a) * h.powi(dimension.h),
+            Cosmology::NonCosmological => unreachable!(),
+        }
     }
 
-    fn read_dimension(&self, set: &Dataset) -> Dimension {
+    fn check_cosmology_available(&self) {
+        match self.cosmology {
+            Cosmology::Cosmological { .. } => {},
+            Cosmology::NonCosmological => panic!("Cosmological units in input file, but no cosmology given. Add cosmology section to parameter file?"),
+        }
+    }
+}
+
+fn is_cosmological(dimension: &Dimension) -> bool {
+    dimension.a != 0 || dimension.h != 0
+}
+
+impl ArepoUnitReader {
+    fn read_raw_dimension(&self, set: &Dataset) -> Dimension {
         let read_attr =
             |ident, error_message| set.attr(ident).expect(error_message).read_scalar().unwrap();
         let length: i32 = read_attr(LENGTH_IDENTIFIER, "No length scale factor in dataset");
@@ -53,6 +61,38 @@ impl UnitReader for ArepoUnitReader {
             amount: 0,
             h,
             a,
+        }
+    }
+}
+
+impl UnitReader for ArepoUnitReader {
+    fn read_scale_factor(&self, set: &Dataset) -> f64 {
+        let dimension = self.read_raw_dimension(set);
+        let cosmology_scale_factor = if is_cosmological(&dimension) {
+            self.get_cosmological_scale_factor(&dimension)
+        } else {
+            1.0
+        };
+        let cgs_to_si = 0.01f64.powi(dimension.length) * 0.001f64.powi(dimension.mass);
+        let cgs: f64 = set
+            .attr(SCALE_FACTOR_IDENTIFIER)
+            .expect("No scale factor in dataset")
+            .read_scalar()
+            .unwrap();
+        cosmology_scale_factor * cgs_to_si * cgs
+    }
+
+    fn read_dimension(&self, set: &Dataset) -> Dimension {
+        let dimension = self.read_raw_dimension(set);
+        if !is_cosmological(&dimension) {
+            dimension
+        } else {
+            self.check_cosmology_available();
+            Dimension {
+                a: 0,
+                h: 0,
+                ..dimension
+            }
         }
     }
 }
