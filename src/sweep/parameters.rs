65b7402148010e820d7f41cf43df293e5d4eada8
tehforsch <mail@tonipeter.de>
2023-06-01 17:34:04 +0200

    add some defaults for sweep params

--- src/sweep/parameters.rs
+++ src/sweep/parameters.rs
@@ -7,16 +7,28 @@ use crate::units::VecDimensionless;
 
 #[raxiom_parameters("sweep")]
 pub struct SweepParameters {
+    /// The number (or concrete list) of directions to use in the
+    /// sweep.
     pub directions: DirectionsSpecification,
-    /// Whether to rotate the direction bins after every (full) sweep step.
-    pub rotate_directions: bool,
+    /// Number of timestep levels to use (the minimum timestep
+    /// is t_max * 2^(-num_timestep_levels))
     pub num_timestep_levels: usize,
-    pub significant_rate_treshold: PhotonRate,
-    pub timestep_safety_factor: Dimensionless,
-    pub check_deadlock: bool,
+    /// Whether to run with periodic boundary conditions.
     pub periodic: bool,
     /// The maximum allowed timestep.
     pub max_timestep: Time,
+    /// Whether to rotate the direction bins after every (full) sweep step.
+    #[serde(default="default_rotate_directions")]
+    pub rotate_directions: bool,
+    #[serde(default)]
+    pub significant_rate_treshold: PhotonRate,
+    #[serde(default = "default_timestep_factor")]
+    pub timestep_safety_factor: Dimensionless,
+    /// Whether to run a deadlock check before each sweep. Potentially
+    /// heavy impact on performance, should only be used during
+    /// debugging.
+    #[serde(default)]
+    pub check_deadlock: bool,
 }
 
 #[raxiom_parameters]
@@ -33,4 +45,12 @@ impl DirectionsSpecification {
             DirectionsSpecification::Explicit(directions) => directions.len(),
         }
     }
+}
+
+fn default_rotate_directions() -> bool {
+    true
+}
+
+fn default_timestep_factor() -> Dimensionless {
+    Dimensionless::percent(10.0)
 }
