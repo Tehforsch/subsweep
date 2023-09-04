719c2e2cba260a80ecdf234b8921fb0a515378d8
tehforsch <mail@tonipeter.de>
2023-09-04 15:53:00 +0200

    slightly refactor 1d test

--- examples/sweep_1d.rs
+++ examples/sweep_1d.rs
@@ -8,7 +8,6 @@ use raxiom::components::Density;
 use raxiom::components::Position;
 use raxiom::components::Source;
 use raxiom::parameters::Cosmology;
-use raxiom::parameters::SimulationParameters;
 use raxiom::prelude::Extent;
 use raxiom::prelude::LocalParticle;
 use raxiom::prelude::Particles;
@@ -28,7 +27,6 @@ use raxiom::units::PhotonFlux;
 use raxiom::units::PhotonRate;
 use raxiom::units::SourceRate;
 use raxiom::units::Temperature;
-use raxiom::units::Time;
 use raxiom::units::VecLength;
 use raxiom::units::Volume;
 
@@ -38,6 +36,12 @@ const BOX_SIZE_MEGAPARSEC: f64 = 1.0;
 struct Params {
     num_particles: usize,
     photon_flux: PhotonFlux,
+}
+
+impl Params {
+    fn cell_size(&self) -> Length {
+        Length::megaparsec(BOX_SIZE_MEGAPARSEC) / self.num_particles as f64
+    }
 }
 
 fn main() {
@@ -55,13 +59,10 @@ fn setup_sweep_sim() -> Simulation {
         .parameter_file_path(&Path::new("params.yml").to_owned())
         .build();
     let params = sim.add_parameter_type_and_get_result::<Params>().clone();
-    add_box_size(&mut sim, params.num_particles);
-    add_grid(&mut sim, params.num_particles);
+    add_box_size(&mut sim, &params);
+    add_grid(&mut sim, &params);
     sim.write_output(true)
         .add_parameters_explicitly(Cosmology::NonCosmological)
-        .add_parameters_explicitly(SimulationParameters {
-            final_time: Some(Time::megayears(1000.0)),
-        })
         .add_startup_system_to_stage(
             StartupStages::InsertComponentsAfterGrid,
             initialize_sweep_components_system,
@@ -71,10 +72,14 @@ fn setup_sweep_sim() -> Simulation {
     sim
 }
 
-fn add_box_size(sim: &mut Simulation, num_particles: usize) {
-    let cell_size = Length::megaparsec(BOX_SIZE_MEGAPARSEC) / num_particles as f64;
+fn add_box_size(sim: &mut Simulation, params: &Params) {
+    let cell_size = params.cell_size();
     let min = VecLength::zero();
-    let max = VecLength::new(cell_size * num_particles as f64, cell_size, cell_size);
+    let max = VecLength::new(
+        Length::megaparsec(BOX_SIZE_MEGAPARSEC),
+        cell_size,
+        cell_size,
+    );
     let extent = Extent {
         min,
         max,
@@ -84,8 +89,8 @@ fn add_box_size(sim: &mut Simulation, num_particles: usize) {
     sim.insert_resource(box_size);
 }
 
-fn add_grid(sim: &mut Simulation, num_particles: usize) {
-    let cell_size = Length::megaparsec(BOX_SIZE_MEGAPARSEC) / num_particles as f64;
+fn add_grid(sim: &mut Simulation, params: &Params) {
+    let cell_size = params.cell_size();
     let grid_setup = move |commands: Commands,
                            box_size: Res<SimulationBox>,
                            world_size: Res<WorldSize>,
@@ -116,10 +121,7 @@ fn add_source_system(
         let source = if entity != most_left.0 {
             SourceRate::zero()
         } else {
-            let num_particles = particles.iter().count();
-            let cell_size = Length::megaparsec(BOX_SIZE_MEGAPARSEC) / num_particles as f64;
-            let photons_per_second = params.photon_flux * cell_size.squared();
-            photons_per_second
+            params.photon_flux * params.cell_size().squared()
         };
         commands.entity(entity).insert(Source(source));
     }
