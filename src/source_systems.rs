64106ca8766dc4ed276aed6e1e71feda7291e9c0
tehforsch <mail@tonipeter.de>
2023-10-19 14:47:29 +0200

    use tree lookup for closest particle in source

--- src/source_systems.rs
+++ src/source_systems.rs
@@ -2,9 +2,10 @@ use bevy_ecs::event::EventWriter;
 use bevy_ecs::prelude::Res;
 use bevy_ecs::prelude::Resource;
 use derive_custom::Named;
+use kiddo::distance::squared_euclidean;
+use kiddo::KdTree;
 use log::debug;
 use mpi::traits::Equivalence;
-use ordered_float::OrderedFloat;
 use serde::Serialize;
 
 use crate::communication::MpiWorld;
@@ -12,6 +13,7 @@ use crate::components;
 use crate::components::Position;
 use crate::domain::DecompositionState;
 use crate::domain::IntoKey;
+use crate::prelude::Float;
 use crate::prelude::Particles;
 use crate::prelude::SimulationBox;
 use crate::prelude::WorldRank;
@@ -47,22 +49,30 @@ pub fn set_source_terms_system(
 ) {
     let mut source_comm = MpiWorld::<Source>::new();
     let all_sources = source_comm.all_gather_varcount(&sources.sources);
+    let mut particles: Vec<_> = particles.iter_mut().collect();
+    let tree: KdTree<Float, 3> = (&particles
+        .iter()
+        .map(|(pos, _)| pos_to_tree_coord(pos))
+        .collect::<Vec<_>>())
+        .into();
     for s in all_sources.iter() {
         let key = s.position.into_key(&*box_);
         let rank = decomposition.get_owning_rank(key);
         if rank == **world_rank {
-            let closest = particles
-                .iter_mut()
-                .map(|(pos, source)| {
-                    let dist = **pos - s.position;
-                    (OrderedFloat(dist.length().value_unchecked()), source)
-                })
-                .min_by_key(|(dist, _)| *dist);
-            let (_, mut source_term) = closest.unwrap();
-            **source_term += s.rate;
+            let (_, index) = tree.nearest_one(&pos_to_tree_coord(&s.position), &squared_euclidean);
+            let (_, ref mut source_term) = &mut particles[index];
+            ***source_term += s.rate;
         }
     }
     let total: SourceRate = all_sources.iter().map(|source| source.rate).sum();
     writer.send(TotalLuminosity(total));
     debug!("Total luminosity: {:+.2e}", total.in_photons_per_second());
+}
+
+fn pos_to_tree_coord(pos: &VecLength) -> [f64; 3] {
+    [
+        pos.x().value_unchecked(),
+        pos.y().value_unchecked(),
+        pos.z().value_unchecked(),
+    ]
 }
